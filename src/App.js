1cabdb335f2a68480ac2176fc0a9d19319c7b042
bperez96 <bernardoperez@utexas.edu>
2022-02-07 11:26:23 -0600

    adding changes to APP.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +27,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
